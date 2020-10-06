df700901167d9394e3de4510ce91d439f37b1699
Ritesh <rathodriteshrock@gmail.com>
2020-10-06 21:31:22 +0530

    Create UserController

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class userController extends Controller
 {
     public function user_login(Request $request)
     {
+        
         
     }
 }
